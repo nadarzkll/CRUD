ab4d9d911506adae76027f4d2890d967bd831a9a
nadarzkll <148950078+nadarzkll@users.noreply.github.com>
2023-10-26 08:48:16 +0700

    index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Website Ataya</title>
+  <title>Website Artikel</title>
   <link href="./css/bootstrap.min.css" rel="stylesheet">
 
 </head>
